958e003b2f71746c0097f447ba8112a5c79c05e1
Ruslan Igorevich <ruslan.igorevich@list.ru>
2018-05-02 20:21:31 +0300

    add slider component

---
+++ src/components/widgets/NewsSlider/slider_templates.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SliderTemplates = (props) => {
+    console.log(props);
+    return (
+        <div>
+            Slider Templates
+        </div>
+    )
+}
+
+export default SliderTemplates;

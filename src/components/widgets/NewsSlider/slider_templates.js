a59a7d9255c0ad5f39a387d4af3207b2445f7ff8
Ruslan Igorevich <ruslan.igorevich@list.ru>
2018-05-02 21:18:11 +0300

    add Slick

--- src/components/widgets/NewsSlider/slider_templates.js
+++ src/components/widgets/NewsSlider/slider_templates.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Slick from 'react-slick';
+
+import './slider.css';
 
 const SliderTemplates = (props) => {
-    console.log(props);
+
+    let template = null;
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+
+    }
+
+    switch(props.type) {
+        case ('featured'):
+            template = props.data.map((item,i) => {
+                return (
+                    <div key={i}>
+                        <div className="featured-item">
+                            <div className="featured-item__img"
+                                style={{
+                                    background:`url(../images/articles/${item.image})`
+                                }}>
+                            </div>
+                            <Link to={`/articles/${item.id}`}>
+                                <div className="featured-item__title">
+                                    {item.title}
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                )
+            })
+            break;
+        default:
+            template = null;
+    }
+
     return (
-        <div>
-            Slider Templates
-        </div>
+        <Slick {...settings}>
+            {template}
+        </Slick>
     )
 }
 

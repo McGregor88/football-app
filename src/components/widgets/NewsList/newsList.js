efc37df169a73235a6a6799444752028836cac95
Ruslan Igorevich <ruslan.igorevich@list.ru>
2018-05-03 18:48:17 +0300

    add btn load-more

--- src/components/widgets/NewsList/newsList.js
+++ src/components/widgets/NewsList/newsList.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 import { URL } from '../../../config';
+import './newsList.css';
 
 class NewsList extends Component {
 
@@ -15,7 +16,11 @@ class NewsList extends Component {
     }
 
     componentWillMount() {
-        axios.get(`${URL}/articles?_start=${this.state.start}&_end=${this.state.end}`)
+        this.request(this.state.start, this.state.end)
+    }
+
+    request = (start,end) => {
+        axios.get(`${URL}/articles?_start=${start}&_end=${end}`)
         .then(response => {
             this.setState({
                 items:[...this.state.items,...response.data]
@@ -23,11 +28,39 @@ class NewsList extends Component {
         })
     }
 
+    loadMore = () => {
+        let end = this.state.end + this.state.amount;
+        this.request(this.state.end,end)
+    }
+
+    renderNews = (type) => {
+        let template = null;
+        switch(type) {
+            case('card'):
+                template = this.state.items.map((item,i) => (
+                    <div key={i}>
+                        <div className="newslist-item">
+                            <Link to={`/articles/${item.id}`}>
+                                <h2>{item.title}</h2>
+                            </Link>
+                        </div>
+                    </div>
+                ));
+                break;
+            default: 
+                template = null;
+        }
+        return template;
+    }
+
     render() {
         console.log(this.state.items)
         return (
             <div>
-                NewsList
+                { this.renderNews(this.props.type) }
+                <div onClick={() => this.loadMore()}>
+                    LOAD MORE
+                </div>
             </div>
         )
     }

15397b53bfae6c4a671073b316d974d2e893e9af
Ruslan Igorevich <ruslan.igorevich@list.ru>
2018-05-06 20:31:02 +0300

    add VideosListTemplate component

--- src/components/widgets/VideosList/videosList.js
+++ src/components/widgets/VideosList/videosList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import { URL } from '../../../config';
+import VideosTemplate from './videosListTemplate';
 import Button from '../Buttons/button';
 import './videosList.css';
 
@@ -18,6 +19,40 @@ class VideosList extends Component {
         return this.props.title ?
             <h3><strong className="text-upper">Football</strong> Videos</h3> 
         : null
+    }
+
+    componentDidMount() {
+        this.request(this.state.start, this.state.end)
+    }
+
+    request = (start,end) => {
+        if(this.state.teams.length < 1) {
+            axios.get(`${URL}/teams`)
+            .then(response => {
+                this.setState({
+                    teams: response.data
+                })
+            })
+        }
+        axios.get(`${URL}/videos?_start=${start}&_end=${end}`)
+        .then(response => {
+            this.setState({
+                videos: [...this.state.videos, ...response.data]
+            })
+        })
+    }
+
+    renderVideos = () => {
+        let template = null;
+
+        switch(this.props.type) {
+            case('card'):
+                template = <VideosTemplate data={this.state.videos} teams={this.state.teams}/>
+                break;
+            default:
+                template = null
+        }
+        return template;
     }
 
     loadMore = () => {
@@ -39,10 +74,11 @@ class VideosList extends Component {
             />
     }
 
-    render() {        
+    render() {
         return (
             <div className="videolist-wrapper">
                 { this.renderTitle() }
+                { this.renderVideos() }
                 { this.renderButton() }
             </div>
         )
---
+++ src/components/widgets/VideosList/videosListTemplate.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import CardInfo from '../CardInfo/cardInfo';
+import './videosList.css';
+
+const VideosTemplate = (props) => {
+    return props.data.map((item,i) => (
+        <Link
+            to={`/videos/${item.id}`}
+            key={i}
+        >
+            <div className="videolist-item__wrapper">
+                <div className="left"
+                    style={{
+                        background: `url(/images/videos/${item.image})`
+                    }}
+                >
+                    <div></div>
+                </div>
+                <div className="right">
+                    <h2>{item.title}</h2>
+                </div>
+            </div>
+        </Link>
+    ))
+}
+
+export default VideosTemplate;

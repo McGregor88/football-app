eb6ef3b0bdb93322770585959b8deba49699e463
Ruslan Igorevich <ruslan.igorevich@list.ru>
2018-05-16 19:25:13 +0300

    news-slider fix

---
+++ src/firebase.js
@@ -0,0 +1,37 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBJDDU6J_plSTGckmTsgwjKYcthxvXiDtE",
+    authDomain: "football-dev-606fb.firebaseapp.com",
+    databaseURL: "https://football-dev-606fb.firebaseio.com",
+    projectId: "football-dev-606fb",
+    storageBucket: "football-dev-606fb.appspot.com",
+    messagingSenderId: "815133201262"
+};
+
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+const firebaseArticles = firebaseDB.ref('articles');
+const firebaseTeams = firebaseDB.ref('teams');
+const firebaseVideos = firebaseDB.ref('videos');
+
+const firebaseLooper = (snapshot) => {
+    const data = [];
+    snapshot.forEach((childSnapshot) => {
+        data.push({
+            ...childSnapshot.val(),
+            id: childSnapshot.key
+        })
+    });
+    return data;
+}
+
+export {
+    firebase,
+    firebaseDB,
+    firebaseArticles,
+    firebaseTeams,
+    firebaseVideos,
+    firebaseLooper
+};
